79fd02f0e5de9c072c17e8c47d0afab0cd809c3a
jeremyrice98 <ricefamily1003@gmail.com>
2020-05-16 11:52:23 -0600

    variable

---
+++ Develop/script.js
@@ -0,0 +1,24 @@
+//Variables
+
+var number = "0123456789";
+var lowerCase = "abcdefghijklmnopqrstuvwxyz";
+var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var special = "!@#$%^&+()"
+
+
+
+
+// Get references to the #generate element
+var generateBtn = document.querySelector("#generate");
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
