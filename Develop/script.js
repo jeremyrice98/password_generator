b7cfe95874e48a69ac52890435f2d87f13359491
jeremyrice98 <ricefamily1003@gmail.com>
2020-05-17 15:04:58 -0600

    revert back

--- Develop/script.js
+++ Develop/script.js
@@ -1,3 +1,18 @@
+    // Get references to the #generate element
+    var generateBtn = document.querySelector("#generate");
+
+    // Write password to the #password input
+    function writePassword() {
+        var password = generatePassword();
+        var passwordText = document.querySelector("#password");
+
+        passwordText.value = password;
+
+    }
+
+    // Add event listener to generate button
+    generateBtn.addEventListener("click", writePassword);
+
 //Variables
 
 var number = "0123456789";
@@ -75,18 +90,5 @@ return password;
 
 }
 
-    // Get references to the #generate element
-    var generateBtn = document.querySelector("#generate");
 
-    // Write password to the #password input
-    function writePassword() {
-        var password = generatePassword();
-        var passwordText = document.querySelector("#password");
-
-        passwordText.value = password;
-
-    }
-
-    // Add event listener to generate button
-    generateBtn.addEventListener("click", writePassword);
 
